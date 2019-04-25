cbbc97370b5fd0f156bcc041dc34546778d3ed75
Juan Angel Reyes <juangel2406@gmail.com>
2019-04-25 02:53:23 -0500

    Calificaciones c/maestro

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -36,29 +36,43 @@ class ApiController extends Controller
     }
 
     function calificaciones(Request $r){
-        $califList_final = collect([]);
+        //Arreglo a mandar
+        $califList = collect([]);
 
+        //Datos de alumno
         $alumno = $r->get('usuario')->persona->alumno;
 
+        //Último grupo del alumno
         $lastGroup = $alumno->grupos()
             ->orderBy('grado', 'DESC')
             ->get()->first();
 
+        //Materias del grupo(grado) actual
         $materias = Materia::where('grado', $lastGroup->grado)
             ->orderBy('nom')->get();
 
         foreach($materias as $materia){
+            /*Calificaciones del alumno relacionadas a la materia
+            *** Contenido: numeros directos ordenados por unidad
+            */
             $materiaCalifs = $materia->calificaciones()
                 ->where('matricula_alumno', $alumno->matricula)
                 ->orderBy('unidad')->pluck('calificacion');
 
-            $califList_final->push(
-                json_encode(['nom' => $materia->nom,
+            $maestro = $materia->asignacion_materias()
+                ->where('grupo_id', $lastGroup->id)->first()
+                ->maestro->persona;
+
+            $maestroNom = $maestro->nom.' '.$maestro->apeP.' '.$maestro->apeM;
+
+            $califList->push(
+                json_encode(['materia' => $materia->nom,
+                             'maestro' => $maestroNom,
                              'prom' => $materiaCalifs->avg(),
                              'califs' => $materiaCalifs->toArray()])
             );
         }
 
-        return $califList_final->toArray();
+        return $califList->toArray();
     }
 }
