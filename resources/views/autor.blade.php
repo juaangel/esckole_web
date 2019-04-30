ddb7a56020dcdfd2ae3cf2d3e4507ee21563d3a3
Jose <juttoliver@gmai.com>
2019-04-30 14:32:02 -0500

    Borrar

---
+++ resources/views/autor.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Esckole</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div style="height: 100vh">
+        <div class="flex-center flex-column">
+            <small class="text-center">EN CONSTRUCCIÓN</small>
+            <h2>Página web del proyecto <strong>"esckole"</strong></h2>
+            <img class="img-fluid animated fadeIn mb-4" src="img/catar-sys.png" alt="CATAR-SYS">
+            <h5 class="animated fadeIn mb-3">Esta página esta conformada por los siguientes componentes:</h5>
+                <a class="animated fadeIn mb-3" href="https://laravel.com/" class="animated fadeIn mb-2">Laravel 5.7</a>
+                <a class="animated fadeIn mb-3" href="https://mdbootstrap.com/" class="animated fadeIn mb-2">Material Design for Bootstrap Free 4.7.1</a>
+        </div>
+    </div>
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <!-- Popper -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+</body>
+
+</html>
