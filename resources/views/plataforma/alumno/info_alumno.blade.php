bde0083714586457a072b2b4581018131921ad6a
Jose <juttoliver@gmai.com>
2019-04-30 14:46:37 -0500

    Aplicando Plantilla

---
+++ resources/views/plataforma/alumno/info_alumno.blade.php
@@ -0,0 +1,14 @@
+@extends('plantillas.plataforma')
+@section('extraCSS')
+    <!--<link rel="stylesheet" href="css/styles/plataforma/???.css">-->
+@endsection
+@section('menu')
+    <a href="#" class="list-group-item list-group-item-action bg-light">Calificaciones</a>
+@endsection
+@section('title') Datos personales @endsection
+@section('content')
+
+
+
+
+@endsection
