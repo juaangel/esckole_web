dfebe24b769ffc75f2bf0e415ec1d6b14bf0aedd
Jose <juttoliver@gmai.com>
2019-05-01 16:51:00 -0500

    Info Alumno

--- app/Http/Controllers/Plataforma/PlataformaController.php
+++ app/Http/Controllers/Plataforma/PlataformaController.php
@@ -125,9 +125,7 @@ class PlataformaController extends Controller
                 ->with('datos', new datosAdmin(Session::get('user')->person_id));
         }
         if($personType == 'alumno'){
-            return view
-            /*('plataforma.alumno.info_alumno')
-             ->with('datos', new datosAlumno(Session::get('user')->user_id));*/
+            return view ('plataforma.alumno.info_alumno');
         }
         if($personType == 'maestro'){
             return view('inConstruct');
