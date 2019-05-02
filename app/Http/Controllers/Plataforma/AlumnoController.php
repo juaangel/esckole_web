75a247d89cff142a83d6fcb650d4651ef345a395
Jose <juttoliver@gmai.com>
2019-05-02 01:33:23 -0500

    calif

--- app/Http/Controllers/Plataforma/AlumnoController.php
+++ app/Http/Controllers/Plataforma/AlumnoController.php
@@ -28,7 +28,7 @@ class AlumnoController extends Controller
         $califList = collect([]);
 
         //Datos de alumno
-        $alumno = Session::get('user')->num->get('usuario')->persona->alumno;
+        $alumno = Session::get('user')->num('usuario')->persona->alumno;
 
         //Último grupo del alumno
         $lastGroup = $alumno->grupos()
