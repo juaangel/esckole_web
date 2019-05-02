108770189e81f2d589f2a08c45028e2bcb925411
Jose <juttoliver@gmai.com>
2019-05-02 02:15:53 -0500

    calif

--- app/Http/Controllers/Plataforma/AlumnoController.php
+++ app/Http/Controllers/Plataforma/AlumnoController.php
@@ -60,6 +60,6 @@ class AlumnoController extends Controller
         }
 
       //  return json_encode($califList->toArray());
-  		 return view ('plataforma.alumno.Calificaciones');
+  		 return view ('plataforma.alumno.Calificaciones')->with('datos', new datosAlumno(Session::get('user')->num));
     }
 }
