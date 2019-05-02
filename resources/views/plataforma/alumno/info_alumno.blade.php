689199c61a0a3aa6581659e9b998f91b7bb96406
Jose <juttoliver@gmai.com>
2019-05-01 19:01:31 -0500

    Info

--- resources/views/plataforma/alumno/info_alumno.blade.php
+++ resources/views/plataforma/alumno/info_alumno.blade.php
@@ -122,11 +122,11 @@
                     </tr>
                     <tr>
                         <th scope="row" colspan="2">Alergías:</th>
-                         <td colspan="2">{{$datos->alergias}}</td>
+                         <td>{{$datos->alergias}}</td>
                     </tr>
                      <tr>
                         <th scope="row" colspan="3">Enfermedades:</th>
-                        <td colspan="2">{{$datos->enfermedades}}</td>
+                        <td>{{$datos->enfermedades}}</td>
                     </tr>
                 </tbody>
             </table>
