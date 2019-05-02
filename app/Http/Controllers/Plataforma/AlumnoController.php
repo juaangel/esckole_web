251aa59fb20a2f35841476c73178056416cae876
Jose <juttoliver@gmai.com>
2019-05-02 01:47:48 -0500

    calif

--- app/Http/Controllers/Plataforma/AlumnoController.php
+++ app/Http/Controllers/Plataforma/AlumnoController.php
@@ -18,6 +18,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Plataforma\calificaciones;
+
 
 class AlumnoController extends Controller
 {
