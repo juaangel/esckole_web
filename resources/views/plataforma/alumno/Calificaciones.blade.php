826ae5695eda5c7ac582493fdd8f69efa3bd9c12
Jose <juttoliver@gmai.com>
2019-05-02 05:00:43 -0500

    calif

--- resources/views/plataforma/alumno/Calificaciones.blade.php
+++ resources/views/plataforma/alumno/Calificaciones.blade.php
@@ -14,8 +14,8 @@
     <tr>
       <th scope="col">Profesor:</th>
       <th scope="col">Materia:</th>
-      <th scope="col">Unidad:</th>
       <th scope="col">Promedio</th>
+      <th scope="col">Unidad:</th>
     </tr>
   </thead>
   <tbody>
