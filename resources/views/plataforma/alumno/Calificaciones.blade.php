3b4f3f2af255cf27cb756d710126513c0c6793e8
Jose <juttoliver@gmai.com>
2019-05-02 03:23:07 -0500

    calif

--- resources/views/plataforma/alumno/Calificaciones.blade.php
+++ resources/views/plataforma/alumno/Calificaciones.blade.php
@@ -8,7 +8,7 @@
 @endsection
 @section('title') Calificaciones @endsection
 @section('content')
-{{$califList}}
+{{$califList->materia}}
     <table class="table table-responsive table-bordered">
         <thead>
             <tr>
