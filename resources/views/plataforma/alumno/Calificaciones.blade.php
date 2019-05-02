bba77cd506949883ff2ccbe20d935f5986beadfd
Jose <juttoliver@gmai.com>
2019-05-01 19:20:44 -0500

    Calif

---
+++ resources/views/plataforma/alumno/Calificaciones.blade.php
@@ -0,0 +1,11 @@
+@extends('plantillas.plataforma')
+@section('extraCSS')
+    <!--<link rel="stylesheet" href="css/styles/plataforma/???.css">-->
+@endsection
+@section('menu')
+    <a href="#" class="list-group-item list-group-item-action bg-light"> Datos personales</a>
+@endsection
+@section('title') Calificaciones @endsection
+@section('content')
+
+@endsection
