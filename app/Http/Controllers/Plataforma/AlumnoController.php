b47ba3cf3234a454874718409b1ae168c581137b
Jose <juttoliver@gmai.com>
2019-05-02 01:47:07 -0500

    calif

--- app/Http/Controllers/Plataforma/AlumnoController.php
+++ app/Http/Controllers/Plataforma/AlumnoController.php
@@ -19,15 +19,15 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\Controller;
 
-
-
 class AlumnoController extends Controller
 {
     function Cali()
     {
 
+    	return calificaciones(Session::get('user')->num);
+
         //Arreglo a mandar
-        $califList = collect([]);
+/*        $califList = collect([]);
 
         //Datos de alumno
         $alumno = Session::get('user')->num;
@@ -61,6 +61,6 @@ class AlumnoController extends Controller
         }
 
         return json_encode($califList->toArray());
-  // return view ('plataforma.alumno.Calificaciones');
+  */// return view ('plataforma.alumno.Calificaciones');
     }
 }
