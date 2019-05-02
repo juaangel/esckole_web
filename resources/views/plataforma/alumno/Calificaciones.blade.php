3f70bc1d576bf7a825aaa250c9077d4ab9668b6f
Jose <juttoliver@gmai.com>
2019-05-02 04:36:26 -0500

    calif

--- resources/views/plataforma/alumno/Calificaciones.blade.php
+++ resources/views/plataforma/alumno/Calificaciones.blade.php
@@ -25,13 +25,8 @@ endforeach-->
         <tbody>
         </tbody>
     </table>
-<div class="table-responsive-sm">
-  <table class="table">
-    ...
-  </table>
-</div>
 
-<div class="table-responsive-md">
+<div class="table-responsive-sm">
   <table class="table">
     		  @foreach ($califList as $user)
             <tr>
@@ -45,17 +40,4 @@ endforeach-->
               @endforeach
   </table>
 </div>
-
-<div class="table-responsive-lg">
-  <table class="table">
-    ...
-  </table>
-</div>
-
-<div class="table-responsive-xl">
-  <table class="table">
-    ...
-  </table>
-</div>
-
 @endsection
